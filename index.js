ce099438a8a0d4b670c14c178669a7c60f777599
Jeremy Fleming <thevisionmachine@gmail.com>
2023-11-11 07:15:08 -0600

    Setting up app skeleton

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("input-field");
+const addBtnEl = document.getElementById("add-button");
+
+addBtnEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value;
+    console.log(inputValue);
+})
