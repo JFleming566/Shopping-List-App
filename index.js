a388153569e774c400471daf2aa0ae5b1fe4b036
Jeremy Fleming <thevisionmachine@gmail.com>
2023-11-14 07:23:11 -0600

    added For Loop to render database items

--- index.js
+++ index.js
@@ -17,19 +17,26 @@ const shoppingListEl = document.getElementById("shopping-list");
 addBtnEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value;
     push(shoppingListInDB, inputValue)
-    appendLi(inputValue)
     clearInputField()
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    let arrayValues = Object.values(snapshot.val())
-    console.log(arrayValues )
+    let itemsArray = Object.values(snapshot.val())
+    clearShoppingListEl()    
+    for (let i = 0; i < itemsArray.length; i++) {
+         console.log(itemsArray[i])
+        appendItemToShoppingListEl(itemsArray[i])
+    }
 })
+
+function clearShoppingListEl () {
+    shoppingListEl.innerHTML = ""    
+}
 
 function clearInputField () {
     inputFieldEl.value = "";
 }
 
-function appendLi(itemValue) {
+function appendItemToShoppingListEl(itemValue) {
       shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
 }
